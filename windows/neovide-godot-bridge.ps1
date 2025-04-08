21d50bf8cea1f780c5b58f62b340d933c2219474
prinzsabishi <michalpedzik.dev@gmail.com>
2025-04-08 11:18:14 +0200

    feat(windows): windows bridge version

---
+++ windows/neovide-godot-bridge.ps1
@@ -0,0 +1,21 @@
+# https://github.com/neovide/neovide/issues/2481#issuecomment-2081810651
+#!/usr/bin/env pwsh
+$server_path = "127.0.0.1:55432"
+$file = $args[0]
+$line = $args[1]
+$col = $args[2]
+$portInUse = [bool](Get-NetTCPConnection -LocalPort 55432 -ErrorAction SilentlyContinue)
+
+# Check if server exists, if not start neovide.
+if (-not $portInUse) {
+    Start-Process -FilePath "neovide" -ArgumentList "$file", "--", "--listen", "$server_path"
+}
+
+# Connect to nvim server and send commands
+# Note: This assumes nvim is in PATH and supports the same server commands
+$remote_command = "<C-\><C-N>:n $file<CR>:call cursor($line,$col)<CR>"
+$additional_args = $args[3..($args.Length-1)]
+
+# Execute nvim with server connection.
+$nvim_args = @("--server", "$server_path", "--remote-send", "$remote_command") + $additional_args
+& nvim $nvim_args
